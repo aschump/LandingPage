61ef7d4ddfd4534ce3e5314a8ac52e0deaffa396
aschumpert <schumpertamber@gmail.com>
2023-04-05 00:11:49 -0400

    fixed highlighting functionality

--- js/app.js
+++ js/app.js
@@ -43,10 +43,6 @@ function addLink(navItem) {
     a.href = `#${navItem.id}`;
     navItem.appendChild(a);
     a.textContent = navItem.id;
-    //a.addEventListener('click', function (event) {
-    //    event.preventDefault();
-    //});
-    //navItem.scrollIntoView();
 }
 
 /**
@@ -57,10 +53,6 @@ function addLink(navItem) {
 
 //---------------------- build the nav------------------
 //-------loop through sections using ForEach----
-//don't need this anymore
-//convert allSections from NodeList into an actual array
-/*const allSectionsArr = Array.from(allSections);*/
-
 allSections.forEach((section) => {
     //create li items for each section
     const current = document.createElement('li');
@@ -76,61 +68,46 @@ allSections.forEach((section) => {
     nav.appendChild(current);
 
     ////add Onclick event listener
-
     current.addEventListener('click', function (event) {
         event.preventDefault();
         section.scrollIntoView({ behavior: "smooth" });
     });
-    //current.addEventListener('click', function (event) {
-    //    event.preventDefault();
-    //    section.scrollIntoView({ behavior: 'smooth' });
-    //});
 });
 
-//query all menu__link
-//for each menu__link make anchor element
-//append
-//^ instead of above, made Helper Method
-
-
-///* add class 'active' to section when near top of viewport*/
-//-----steps---
-//create scroll event listener on window object
-window.addEventListener('scroll', function () {
-    //iterate over allSections variable to access each section
-    //for (i = 0; i < allSections.Length; i++) {
-    allSections.forEach(function (section) {
-        //retrieve the section's position on page
-        const currentPos = section.getBoundingClientRect();
-        //use position to check if section is visible within viewport
-        const isVisible = currentPos.top > 0
-            && currentPos.left > 0
-            && currentPos.right <= window.innerWidth
-            && currentPos.bottom <= window.innerHeight;
-        //if so, add your-active-class to that section
-        if (isVisible) {
-            //use classList to add the class
-            section.classList.add('your-active-class');
-        }
-        //else, remove active class from the section
-        else {
-            //use classList to remove the class
-            section.classList.remove('your-active-class');
-        }
-
-    });
-});
-
-    // Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
-*/
+    /**
+     * End Main Functions
+     * Begin Events
+     *
+    */
 
     // Build menu
 
     // Scroll to section on link click
 
     // Set sections as active
+
+    //-------Highlight Active Section---------------
+    //create scroll event listener on window object
+    window.addEventListener('scroll', function () {
+        //iterate over allSections variable to access each section
+        allSections.forEach(function (section) {
+            //retrieve the section's position on page
+            const currentPos = section.getBoundingClientRect();
+            //use position to check if section is visible within viewport
+            const isVisible = currentPos.top > 0
+                && currentPos.left > 0
+                && currentPos.right <= window.innerWidth
+                && currentPos.bottom <= window.innerHeight;
+            //if so, add your-active-class to that section
+            if (isVisible) {
+                //use classList to add the class
+                section.classList.add('your-active-class');
+            }
+            //else, remove active class from the section
+            else {
+                //use classList to remove the class
+                section.classList.remove('your-active-class');
+            }
+        });
+
+    });
